e81ff193652d8c1aa35865cde75e88165eebb2c6
lida <kurogiri.dabi@gmail.com>
2022-06-08 10:41:53 +0300

    comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,37 +1,56 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        int[][] data = {
-                {},
-                {1},
-                {0, 3, 2, 1},
-                {4, 3, 2, 1, 0},
-                {6, 8, 3, 123, 5, 4, 1, 2, 0, 9, 7},
-        };
-        for (int[] arr : data) {
-            System.out.print(Arrays.toString(arr) + " => ");
-            SelectionSorter.sort(arr);
-            System.out.println("отсортированный массив" + Arrays.toString(arr));
+class Sort {
+    public static void quickSort(int[] array, int left, int right) {
+        int mean = left + (right - left) / 2;
+        int support = array[mean];
+        int i = left, j = right;
+
+        while (i <= j) {
+            while (array[i] > support) {
+                i++;
+            }
+            while (array[j] < support) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
         }
-    }
-}
-class SelectionSorter {
-    public static void sort(int[] array) {
-        for (int i = 0; i < array.length; i++) { // i - номер текущего шага
-            int pos = i;
-            int min = array[i];
-
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < min) {
-                    pos = j; // pos - индекс наименьшего элемента
-                    min = array[j];
+        if (left < j)
+            quickSort(array, left, j);
+        if (right > i)
+            quickSort(array, i, right);
+        for(int i = 0; i < 0; i++){
+            for(int j = 0; j < 0; j++){
+                if( i==j ){
+                    swap()
                 }
             }
-            array[pos] = array[i];
-            array[i] = min; // меняем местами наименьший с array[i]
         }
+    }
+    public static void main (String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите длину массива ");
+        int n = scanner.nextInt();
+        int myArray[] = new int[n];
+        System.out.print("Введите элементы массива:");
+        for (int i = 0; i < n; i++) {
+            myArray[i] = scanner.nextInt();
+        }
+        for (int j = 0; j < n; j++) {
+        }
+        int left = myArray.length - 5;
+        int right = myArray.length - 1;
+        quickSort(myArray, left, right);
+
+        System.out.println("Отсортированный массив: " + Arrays.toString(myArray) );
     }
 }
